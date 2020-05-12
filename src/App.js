7d1d5dd52bf2bf2b1e65b9aac19d200392e867ac
kelli <kellikells@gmail.com>
2020-05-11 22:40:22 -0700

    changed App.js and ImageCard component to get image information, but still returning problems

--- src/App.js
+++ src/App.js
@@ -1,41 +1,34 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 import ImageCard from "./components/ImageCard";
 import Main from "./components/Main";
-
 import myCards from "./myCards.json";
 
-class App extends Component {
+console.log(myCards);
+
+function App() {
 
     // --- setting this.state.myCards to the myCards json array
-    state = {
-        myCards
-    };
+    // state = {
+    //     myCards
+    // };
 
     // --- change property of clicked 
     // isClicked =
 
-    // --- forEach: render a Card component for each myCard object
-    render() {
-        return (
-            <Main>
-                {/* <Navbar />
-                <Jumbotron /> */}
-                <Navbar></Navbar>
-                <Jumbotron></Jumbotron>
+    return (
+        <Main>
+            <Navbar />
+            <Jumbotron />
 
-                {/* {this.state.myCards.forEach(picture => ( */}
-                {myCards.forEach(picture => (
-                    <ImageCard
-                        // isClicked={this.isClicked}
-                        id={picture.id}
-                        image={picture.image}
-                    />
-                ))}
-            </Main>
-        );
-    }
+            {myCards.forEach(picture => (
+                <ImageCard picture={picture} />
+            ))}
+        </Main>
+    );
 }
+
 
 export default App;
--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./style.css";
 
+// const ImageCard = () => (
+//     <div className="oneCard">
+//         <div className="img-container">
+//             <img alt={this.id} src={this.image} />
+//         </div>
+//     </div>
+// );
+
 function ImageCard(props) {
     return (
         <div className="oneCard">
             <div className="img-container">
-                <img alt={props.id} src={props.image} />
+                <img alt={props.id} src={props.image} key={props.id} />
             </div>
         </div>
     );
